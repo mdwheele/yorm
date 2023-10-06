0d16218ad5c014827bbc7368cf7cf8a71d63e16f
Dustin Wheeler <dustin.wheeler@nutanix.com>
2023-10-06 17:31:38 -0400

    Add more transaction tests

--- tests/transaction.test.ts
+++ tests/transaction.test.ts
@@ -36,9 +36,43 @@ test('Model classes knex instance are rebound with transaction without affecting
 })
 
 test('Transactions are implicitly committed', async () => {
+  let  userId
+
   await transaction(User, Post, async (User, Post) => {
     const user = await User.create({ name: `Susan O'Malley`, username: 'susan.omalley@nutanix.com' })
+
+    userId = user.id
+
+    await Post.create({ user_id: user.id, title: `My first post`, content: `Lorem ipsum!` })
   })
+
+  const user = await User.find(userId)
+  const [post] = await Post.where({ user_id: userId })
+  
+  expect(user.name).toContain('Susan')
+  expect(post.title).toBe('My first post')
+
+  await Post.delete()
+  await User.delete()
+})
+
+test('Transactions are implicitly rolled back on any failure', async () => {
+  try {
+    await transaction(User, Post, async (User, Post) => {
+      const user = await User.create({ name: `Susan O'Malley`, username: 'susan.omalley@nutanix.com' })
+      await Post.create({ user_id: user.id, title: `My first post`, content: `Lorem ipsum!` })
+      
+      expect(await User.count()).toBe(1)
+      expect(await Post.count()).toBe(1)
+  
+      throw new Error('Any exception from anywhere in this callback will roll the transaction back')
+    })
+  } catch (error) {
+    expect(error.message).toContain('Any exception')
+  }
+
+  expect(await User.count()).toBe(0)
+  expect(await Post.count()).toBe(0)
 })
 
 afterAll(() => {
